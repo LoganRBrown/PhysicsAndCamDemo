f6278ff5a33365d846edb99c180f6dd5e7a26777
Logan Brown <brownlogan648@gmail.com>
2018-10-04 11:11:37 -0400

    Started Player Ability
    started working on the unique player ability

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,6 +6,38 @@ public class PlayerMovement : MonoBehaviour {
 
     public Transform cam;
 
+    public float health = 100;
+
+    public float mana = 100;
+
+    public float stamina = 50;
+
+    #region UniqueAbility
+
+    string playerAbility;
+
+    /// <summary>
+    /// This is the Unique ability variable that determines the percent to be applied to the specifc attributes determined by the rest of the ability.
+    /// </summary>
+    float[] abilityPercent = new float[] { 25, 50, 75, 100 };
+
+    /// <summary>
+    /// This is the Unique ability variable that determines what typing the ability will have.  
+    /// </summary>
+    string[] abilityType = new string[] { "Discount", "Invulnerability" };
+
+    /// <summary>
+    /// This is the Unique ability variable that determines where to apply the ability.
+    /// </summary>
+    string[] abilityAttribute = new string[] { "Mana", "Health", "Stamina" };
+
+    /// <summary>
+    /// This is the unique ability variable that determines how long the ability lasts for in seconds.
+    /// </summary>
+    float[] abilityDuration = new float[] { .25f, .5f, .75f, 1 };
+#endregion
+
+
     Rigidbody body;
 
 	// Use this for initialization
@@ -42,4 +74,25 @@ public class PlayerMovement : MonoBehaviour {
         //body.AddForce(move * Time.deltaTime * 1000);
         body.AddTorque(torque * Time.deltaTime * 1000);
 	}
+
+    void AssignPlayerAbility()
+    {
+        int abilPerc = Random.Range(-1, 5);
+
+        int abilType = Random.Range(-1, 2);
+
+        int abilAtt = Random.Range(-1, 3);
+
+        int abilLength = Random.Range(-1, 5);
+
+        float percent = abilityPercent[abilPerc];
+
+        string type = abilityType[abilType];
+
+        string attribute = abilityAttribute[abilAtt];
+
+        float duration = abilityDuration[abilLength];
+
+        playerAbility = type + percent + attribute + duration;
+    }
 }
